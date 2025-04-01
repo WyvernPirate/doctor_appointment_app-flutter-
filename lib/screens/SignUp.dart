9e88989e058875309b46f9d4e0413877e086057a
Phemelo Moloi <moloieric80@gmail.com>
2025-04-02 01:10:23 +0200

    Adds sign up screen
    Implements the initial sign-up screen with a basic UI.
    This screen provides a placeholder for the future sign-up functionality.

---
+++ lib/screens/SignUp.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('Sign Up Screen'),
+      ),
+    );
+  }
+}
