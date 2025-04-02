e8ba628f566ec266d6131a71da87c287d456a42b
Phemelo Moloi <moloieric80@gmail.com>
2025-04-02 19:52:49 +0200

    Implements Sign Up screen UI
    Adds the UI elements for the sign-up screen, including text fields for full name, email, password, and confirm password.
    Implements basic styling and layout for the sign-up form.
    Adds social login buttons (Facebook, Google, Apple).
    Adds navigation to the Home screen after successful sign-up (temporary).

--- lib/screens/SignUp.dart
+++ lib/screens/SignUp.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+import 'Home.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -11,22 +14,297 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign Up'),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-      child: Column(children: [
-        _signUpSection()
-      ]),
-
-      )
+      appBar: AppBar(title: const Text('Sign Up'), centerTitle: true),
+      body: SingleChildScrollView(child: Column(children: [_signUpSection()])),
     );
   }
 
   Column _signUpSection() {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 50),
+        const Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Text(
+            'Sign Up',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 5, left: 15),
+          child: Text(
+            'Full Name',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w800,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(6),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: const Color.fromARGB(
+                  255,
+                  207,
+                  191,
+                  193,
+                ).withOpacity(0.5),
+                blurRadius: 20,
+                spreadRadius: 1,
+              ),
+            ],
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 16,
+              ),
+              hintText: 'Enter your full name',
+              hintStyle: const TextStyle(
+                color: Color(0xffDDDADA),
+                fontSize: 14,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 20, left: 15),
+          child: Text(
+            'Email Address',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w800,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(6),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: const Color.fromARGB(
+                  255,
+                  207,
+                  191,
+                  193,
+                ).withOpacity(0.5),
+                blurRadius: 20,
+                spreadRadius: 1,
+              ),
+            ],
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 16,
+              ),
+              hintText: 'Enter your email address',
+              hintStyle: const TextStyle(
+                color: Color(0xffDDDADA),
+                fontSize: 14,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 20, left: 15),
+          child: Text(
+            'Create Password',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w800,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(6),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: const Color.fromARGB(
+                  255,
+                  207,
+                  191,
+                  193,
+                ).withOpacity(0.5),
+                blurRadius: 20,
+                spreadRadius: 1,
+              ),
+            ],
+          ),
+          child: TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 16,
+              ),
+              hintText: 'Enter your password',
+              hintStyle: const TextStyle(
+                color: Color(0xffDDDADA),
+                fontSize: 14,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 20, left: 15),
+          child: Text(
+            'Confirm Password',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w800,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(6),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: const Color.fromARGB(
+                  255,
+                  207,
+                  191,
+                  193,
+                ).withOpacity(0.5),
+                blurRadius: 20,
+                spreadRadius: 1,
+              ),
+            ],
+          ),
+          child: TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 16,
+              ),
+              hintText: 'Confirm your password',
+              hintStyle: const TextStyle(
+                color: Color(0xffDDDADA),
+                fontSize: 14,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        ),
 
+        //TODO: implement a terms and contions check
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: SizedBox(
+            width: double.infinity,
+            height: 50,
+            child: ElevatedButton(
+              onPressed: () {
+                // TODO: Handle sign up
+
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Home(),
+                  ), //login for now
+                );
+
+                print("Sign up button pressed");
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'Sign Up',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 20.0),
+          child: Row(
+            children: [
+              Expanded(child: Divider(thickness: 1)),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text("Or Register with"),
+              ),
+              Expanded(child: Divider(thickness: 1)),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SignInButton(
+                Buttons.FacebookNew,
+                onPressed: () {
+                  // TODO: Handle Facebook login
+                  print("Facebook login pressed");
+                },
+              ),
+              SignInButton(
+                Buttons.GoogleDark,
+                onPressed: () {
+                  // TODO: Handle Google login
+                  print("Google login pressed");
+                },
+              ),
+              SignInButton(
+                Buttons.AppleDark,
+                onPressed: () {
+                  // TODO: Handle Apple login
+                  print("Apple login pressed");
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
