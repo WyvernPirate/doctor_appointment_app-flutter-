84f513aac51e74306b5e4146d756b712292569c2
Phemelo Moloi <moloieric80@gmail.com>
2025-04-02 19:39:06 +0200

    Adds logout functionality to home screen
    Implements a logout feature, allowing users to securely exit the application.
    
    Persists login state using SharedPreferences and navigates users back to the login screen upon logout.
    Also resets the 'isGuest' flag on logout.
    Displays a message indicating whether the user is logged in as a guest.

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,7 @@
+// Home.dart
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'InitLogin.dart'; // Import your login screen
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,23 +11,56 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _isGuest = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadGuestStatus();
+  }
+
+  Future<void> _loadGuestStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _isGuest = prefs.getBool('isGuest') ?? false;
+    });
+  }
+
+  Future<void> _handleLogout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isLoggedIn', false);
+    await prefs.setBool('isGuest', false); // Reset isGuest on logout
+
+    // Navigate back to the login screen
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const InitLogin()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Screen'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: _handleLogout, // Call the logout function
+          ),
+        ],
       ),
-      body:Column(
-        children: [
-        _searchSection()
+      body: Column(children: [
+        _searchSection(),
+        _isGuest
+            ? const Text("You are in guest mode")
+            : const Text("You are logged in")
       ]),
-      );
+    );
   }
 
   Column _searchSection() {
-    return Column(
-
-    );
+    return Column();
   }
 }
