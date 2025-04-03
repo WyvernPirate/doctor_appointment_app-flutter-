02139217ec5d043e01cb94dd6868738f3ee2d40e
Phemelo Moloi <moloieric80@gmail.com>
2025-04-03 02:51:59 +0200

    Initializes Firebase for authentication.
    Sets up Firebase to enable user authentication features.
    This includes initializing Firebase with the appropriate options for the current platform.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/Home.dart'; // Import your Home screen
 import 'screens/InitLogin.dart'; // Import your login screen
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Required for shared_preferences
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // Check login status
+
+
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false; // Get login status, default to false
   bool isGuest = prefs.getBool('isGuest') ?? false; // Get guest status, default to false
@@ -16,8 +24,7 @@ class MyApp extends StatelessWidget {
   final bool isLoggedIn;
   final bool isGuest;
 
-  const MyApp({Key? key, required this.isLoggedIn, required this.isGuest})
-      : super(key: key);
+  const MyApp({super.key, required this.isLoggedIn, required this.isGuest});
 
   @override
   Widget build(BuildContext context) {
