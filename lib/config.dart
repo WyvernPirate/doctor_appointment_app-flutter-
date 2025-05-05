25d6753af283c375644447a2819a30469a6fb804
Phemelo Moloi <moloieric80@gmail.com>
2025-05-05 13:29:37 +0200

    Adds app configuration
    Introduces a configuration class to manage environment variables.
    
    This allows easy access to the Google Maps API key via `AppConfig.googleMapsApiKey`, with a fallback mechanism to prevent crashes if the environment variable is not set.

---
+++ lib/config.dart
@@ -0,0 +1,9 @@
+// lib/config.dart
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AppConfig {
+  // Use a getter to access the environment variable
+  static String get googleMapsApiKey {
+    return dotenv.env['GOOGLE_MAPS_API_KEY'] ?? 'YOUR_DEFAULT_OR_ERROR_KEY'; // Provide a fallback or handle error
+  }
+}
