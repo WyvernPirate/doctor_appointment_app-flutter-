47b2b716fe82fde758b947c69022be311d97cd2a
Phemelo Moloi <moloieric80@gmail.com>
2025-05-06 14:51:47 +0200

    Ensures Google Maps API key is accessible
    Makes the Google Maps API key accessible via an environment variable.
    
    This change centralizes the configuration and allows easier
    management of the API key across different environments.

--- lib/config.dart
+++ lib/config.dart
@@ -1,9 +1,8 @@
-// lib/config.dart
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
   // Use a getter to access the environment variable
   static String get googleMapsApiKey {
-    return dotenv.env['GOOGLE_MAPS_API_KEY'] ?? 'YOUR_DEFAULT_OR_ERROR_KEY'; // Provide a fallback or handle error
+    return dotenv.env['GOOGLE_MAPS_API_KEY'] ?? 'YOUR_DEFAULT_OR_ERROR_KEY'; 
   }
 }
